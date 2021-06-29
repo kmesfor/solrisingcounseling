eac5946a53bdb7b6f7155a04f4310e986597e5de
kmesfor <kianmesforush@gmail.com>
2021-06-29 13:28:32 -0400

    add theme toggle (unfinished)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+import './css-vars/CSSVars.css'
 import Home from './pages'
 import SigninPage from './pages/signin'
+import ThemeToggle from './components/ThemeToggle'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
 	return (
+		<>
 		<Router>
 			<Switch>
 				<Route path="/" component={Home} exact />
 				<Route path="/signin" component={SigninPage} exact />
 			</Switch>
 		</Router>
+		<ThemeToggle />
+		</>
 	);
 }
 
---
+++ src/components/ThemeToggle/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ThemeToggle = () => {
+	return (
+		<div>
+			<input type='checkbox'></input>
+		</div>
+	)
+}
+
+export default ThemeToggle
